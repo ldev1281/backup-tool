3342b3ca2d7555d910e2fb87feb23aad7908f753
Andrew Kornilov <akornilov@gmail.com>
2025-07-16 03:49:48 +0300

    Always restore required metadata files (however it'd be better to skip rsync conf files if --apps is defined)

--- usr/lib/limbo-backup/restore.modules.d/04-rsync.bash
+++ usr/lib/limbo-backup/restore.modules.d/04-rsync.bash
@@ -7,6 +7,9 @@ set -euo pipefail
 source /usr/lib/limbo-backup/restore.defaults.bash
 source /etc/limbo-backup/restore.conf.bash
 
+#
+METADATA_DIR=".limbo-backup"
+METADATA_INCLUDE_PATHS=("/etc")
 #########################################################################
 
 #
@@ -19,10 +22,44 @@ mkdir -p "$VERSIONS_ARTEFACTS_DIR"
 
 #########################################################################
 
+# Default rsync options for restoring
+RSYNC_OPTS=(-aR --delete)
+
+if [[ -z "$RESTORE_OVERWRITE" ]]; then
+  RSYNC_OPTS+=("--backup" "--backup-dir=$VERSIONS_ARTEFACTS_DIR")
+fi
+
+####################
+
+# Restore all metadata files from METADATA_INCLUDE_PATHS
+ARTEFACT_PATH="$RSYNC_ARTEFACTS_DIR/$METADATA_DIR"
+
+logger -p user.info -t "$LOGGER_TAG" "Starting restore for metadata $METADATA_DIR (${METADATA_INCLUDE_PATHS[@]})"
+
+pushd "$ARTEFACT_PATH" >/dev/null
+
+for INCLUDE_PATH in "${METADATA_INCLUDE_PATHS[@]}"; do
+  RELATIVE_PATH="${INCLUDE_PATH#/}"
+
+  if [[ ! -e "$RELATIVE_PATH" ]]; then
+    logger -p user.warn -t "$LOGGER_TAG" "Skip missing: $ARTEFACT_PATH/$RELATIVE_PATH"
+    continue
+  fi
+
+  logger -p user.info -t "$LOGGER_TAG" "Syncing: $RELATIVE_PATH -> /"
+  rsync "${RSYNC_OPTS[@]}" "$RELATIVE_PATH" /
+done
+
+popd >/dev/null
+
+logger -p user.info -t "$LOGGER_TAG" "Restore completed for metadata $METADATA_DIR (${METADATA_INCLUDE_PATHS[@]})"
+
+#########################################################################
+
 # Find all matching config files and sort by name
 mapfile -t RSYNC_CONFIG_FILES < <(find "$RSYNC_CONFIG_DIR" -type f -name '[0-9][0-9]-*.conf.bash' | sort)
 
-# Restore all artefacts or particular
+# Restore all artefacts or particular ones
 if [[ ${#RESTORE_APPS[@]} -eq 0 ]]; then
   logger -p user.err -t "$LOGGER_TAG" "RESTORE_APPS is not set in restore.conf.bash"
   exit 1
@@ -32,8 +69,8 @@ declare RESTORE_ARTEFACT=0
 
 for CONFIG in "${RSYNC_CONFIG_FILES[@]}"; do
   CONFIG_FILENAME="$(basename "$CONFIG")"
-  ARTEFACT_NAME="${CONFIG_FILENAME#*-}"             # Remove NN-
-  ARTEFACT_NAME="${ARTEFACT_NAME%.conf.bash}"       # Remove .conf.bash
+  ARTEFACT_NAME="${CONFIG_FILENAME#*-}"       # Remove NN-
+  ARTEFACT_NAME="${ARTEFACT_NAME%.conf.bash}" # Remove .conf.bash
   ARTEFACT_PATH="$RSYNC_ARTEFACTS_DIR/$ARTEFACT_NAME"
 
   if [[ "${RESTORE_APPS[0]}" != "*" ]]; then
@@ -46,7 +83,7 @@ for CONFIG in "${RSYNC_CONFIG_FILES[@]}"; do
     done
 
     if [[ "$RESTORE_ARTEFACT" -eq 0 ]]; then
-      logger -p user.info -t "$LOGGER_TAG" "Skipping restore for: $ARTEFACT_NAME"    
+      logger -p user.info -t "$LOGGER_TAG" "Skipping restore for: $ARTEFACT_NAME"
       continue
     fi
   fi
@@ -64,13 +101,7 @@ for CONFIG in "${RSYNC_CONFIG_FILES[@]}"; do
     eval "$CMD_BEFORE_RESTORE"
   fi
 
-  RSYNC_OPTS=(-aR --delete)
-
-  if [[ -z "$RESTORE_OVERWRITE" ]]; then
-    RSYNC_OPTS+=("--backup" "--backup-dir=$VERSIONS_ARTEFACTS_DIR")
-  fi
-
-  pushd "$ARTEFACT_PATH" > /dev/null
+  pushd "$ARTEFACT_PATH" >/dev/null
 
   for INCLUDE_PATH in "${INCLUDE_PATHS[@]}"; do
     RELATIVE_PATH="${INCLUDE_PATH#/}"
@@ -84,7 +115,7 @@ for CONFIG in "${RSYNC_CONFIG_FILES[@]}"; do
     rsync "${RSYNC_OPTS[@]}" "$RELATIVE_PATH" /
   done
 
-  popd > /dev/null
+  popd >/dev/null
 
   if [[ -n "${CMD_AFTER_RESTORE:-}" ]]; then
     logger -p user.info -t "$LOGGER_TAG" "Running CMD_AFTER_RESTORE for: $ARTEFACT_NAME"
